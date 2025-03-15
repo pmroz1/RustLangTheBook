24087e293bf4e574ac6a13eeed5bca0a64333058
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-03-15 13:40:55 +0100

    refactor: extract user input logic into a separate function

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let guess = get_user_input();
 
     println!("You guessed: {}", guess);
+}
+
+
+fn get_user_input() -> String{
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).expect("Failed to read line");
+    user_input.trim().to_string();
+    return user_input;
 }
