c8d55cbd77f18851c6f043a380316fdda0fe2e66
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-03-20 15:18:15 +0100

    refactor: improve comments and formatting in Ownership main function

--- Ownership/src/main.rs
+++ Ownership/src/main.rs
@@ -1,23 +1,27 @@
 fn main() {
-    let s = String::from("hello");  // s comes into scope
+    let s = String::from("hello"); // s comes into scope
 
-    takes_ownership(s);             // s's value moves into the function...
-                                    // ... and so is no longer valid here
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and so is no longer valid here
 
-    let x = 5;                      // x comes into scope
+    // let a = s; it will break code because s was moved to takes_ownership
+    // println!("{s}"); it will break code because s was moved to takes_ownership
 
-    makes_copy(x);                  // because i32 implements the Copy trait,
-                                    // x does NOT move into the function,
-    println!("{}", x);              // so it's okay to use x afterward
+    let x = 5; // x comes into scope
 
+    makes_copy(x); // because i32 implements the Copy trait,
+                   // x does NOT move into the function,
+    println!("{}", x); // so it's okay to use x afterward
 } // Here, x goes out of scope, then s. But because s's value was moved, nothing
   // special happens.
 
-fn takes_ownership(some_string: String) { // some_string comes into scope
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
     println!("{some_string}");
 } // Here, some_string goes out of scope and `drop` is called. The backing
   // memory is freed.
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
     println!("{some_integer}");
 } // Here, some_integer goes out of scope. Nothing special happens.
